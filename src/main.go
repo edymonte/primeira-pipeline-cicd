f24bb96bf128a66274aeeeab7b112e3bbca7f936
Edilson Monteiro <edymonte@gmail.com>
2024-08-03 19:30:38 +0000

    Validando

--- src/main.go
+++ src/main.go
@@ -9,7 +9,7 @@ import (
 func index(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "text/plain")
-	fmt.Fprintf(w, "Aplicacao exemplo - Monteiro 2.0")
+	fmt.Fprintf(w, "Aplicacao exemplo - Wounderful World of Go\n")
 }
 
 func main() {
