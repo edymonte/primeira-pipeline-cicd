a09a8c03f0667a517d11c9e08b70b50908aeb178
Edilson Monteiro <51175821+edymonte@users.noreply.github.com>
2024-09-21 19:42:28 +0000

    Update main.go

--- src/main.go
+++ src/main.go
@@ -9,7 +9,7 @@ import (
 func index(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "text/plain")
-	fmt.Fprintf(w, "Aplicacao - Edilson Monteiro ")
+	fmt.Fprintf(w, "Aplicacao - Evolução Continua ")
 }
 
 func main() {
