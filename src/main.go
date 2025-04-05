9c117baa95a7d5510edf83644b131e676124d7a6
Edilson Monteiro <edymonte@gmail.com>
2025-04-05 20:10:44 +0000

    Subindo aks

--- src/main.go
+++ src/main.go
@@ -9,7 +9,7 @@ import (
 func index(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "text/plain")
-	fmt.Fprintf(w, "Aplicacao - Evolucao Continua exemplo - 2.2")
+	fmt.Fprintf(w, "Aplicacao - Evolucao Continua exemplo - 2.4")
 }
 
 func main() {
