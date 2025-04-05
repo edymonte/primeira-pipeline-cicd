17fcf79be8782e2790dba141a9a21a0fc8a38eeb
Edilson Monteiro <edymonte@gmail.com>
2025-04-05 19:20:26 +0000

    Alterando meu codigo

--- src/main.go
+++ src/main.go
@@ -9,7 +9,7 @@ import (
 func index(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "text/plain")
-	fmt.Fprintf(w, "Aplicacao - Evolucao Continua exemplo - 2.1")
+	fmt.Fprintf(w, "Aplicacao - Evolucao Continua exemplo - 2.2")
 }
 
 func main() {
