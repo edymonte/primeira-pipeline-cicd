33a455be2f0e87c30ae4d4afa156a99f63e63398
Edilson Monteiro <51175821+edymonte@users.noreply.github.com>
2025-04-24 10:00:11 -0300

    Update main.go

--- src/main.go
+++ src/main.go
@@ -9,7 +9,7 @@ import (
 func index(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "text/plain")
-	fmt.Fprintf(w, "Aplicacao - Evolucao Continua exemplo - 2.4")
+	fmt.Fprintf(w, "Aplicacao - Evolucao Continua exemplo - 2.5")
 }
 
 func main() {
